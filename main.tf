bc1fe46e2c98efa6449774c70a1fb411e07e770e
vineeth soma <vineeth.soma@esadvisory.com>
2018-06-22 08:39:16 -0400

    ec2 and security roup is working

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
 provider "aws" {
-    region = "us-east-1"
+    region  = "us-east-1"
+}
+
+resource "aws_instance" "terra-test" {
+    ami             = "ami-40d28157"
+    instance_type   = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    
+    user_data       = <<-EOF
+                        #! /bin/sh
+                        echo "Hello World" > index.html
+                        nohup busybox httpd -f -p 8080 &
+                        EOF
+    
+    tags {
+        Name    = "terra-test"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terra-test-instance"
+
+    ingress {
+        from_port   = "8080"
+        to_port     = "8080"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
